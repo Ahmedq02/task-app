5dc1d7f4a56105ef03a18eaf6c34b834298cfec0
Ahmedq02 <ahmed.abdulq02@gmail.com>
2024-08-28 17:45:50 +0100

    Added the high priority task count and task table to finish the dashboard

--- dashboard/views.py
+++ dashboard/views.py
@@ -8,18 +8,20 @@ from entries.models import Task
 def display_dashboard(request):
     today = datetime.today()
     next_30_days = today + timedelta(days=30)
+    # get the tasks due in the next 30 days
+    next_tasks = Task.objects.filter(due_by__range=[today, next_30_days])
 
-    tasks = Task.objects.filter(due_by__range=[today, next_30_days])
-
+    # create the line chart
     line_labels = []
     line_data = []
 
     for i in range(31):
         day = today + timedelta(days=i)
         line_labels.append(day.strftime('%Y-%m-%d'))
-        day_task_count = tasks.filter(due_by__date=day).count()
+        day_task_count = next_tasks.filter(due_by__date=day).count()
         line_data.append(day_task_count)
     
+    # create the pie chart
     pie_labels = ["Low", "Medium", "High"]
     pie_data = []
 
@@ -27,11 +29,18 @@ def display_dashboard(request):
         count = Task.objects.filter(priority=i).count()
         pie_data.append(count)
     
+    # gets the high priority tasks that are due in the next 30 days
+    urgent_tasks = next_tasks.filter(priority=3).count()
+
+    tasks = Task.objects.all()
+    
     context = {
         "line_labels": json.dumps(line_labels),
         "line_data": json.dumps(line_data),
         "pie_labels": json.dumps(pie_labels),
         "pie_data": json.dumps(pie_data),
+        "urgent_tasks": urgent_tasks,
+        "tasks": tasks,
     }
 
     return render(request, "dashboard/index.html", context)
