64297781ff542ba5b26c466f6d8dab2a0149b814
Andrei Matei <29061011+andmatei@users.noreply.github.com>
2024-09-18 11:31:15 +0300

    Add pre-commit hook (#4)
    * fix: missed mapping in pre-commit file
    
    * style: apply pre-commit hook to all files

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,6 +1,4 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [
-    path("", views.display_dashboard, name="dashboard")
-]
+urlpatterns = [path("", views.display_dashboard, name="dashboard")]
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,8 +1,8 @@
 from datetime import datetime, timedelta
 import json
 from django.shortcuts import render
-from django.http import HttpResponse
 from entries.models import Task
+
 
 # Create your views here.
 def display_dashboard(request):
@@ -27,23 +27,23 @@ def display_dashboard(request):
 
     for i in range(31):
         day = today + timedelta(days=i)
-        line_labels.append(day.strftime('%Y-%m-%d'))
+        line_labels.append(day.strftime("%Y-%m-%d"))
         day_task_count = next_tasks.filter(due_by__date=day).count()
         line_data.append(day_task_count)
-    
+
     # create the pie chart
     pie_labels = ["Low", "Medium", "High"]
     pie_data = []
 
-    for i in range(1,4):
+    for i in range(1, 4):
         count = Task.objects.filter(priority=i).count()
         pie_data.append(count)
-    
+
     # gets the urgent tasks that are due in the next 30 days
     urgent_tasks = next_tasks.filter(is_urgent=True).count()
 
     tasks = Task.objects.all()
-    
+
     context = {
         "line_labels": json.dumps(line_labels),
         "line_data": json.dumps(line_data),
