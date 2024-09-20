33920217ec9e183aac588eb37b5ec0403c1519d2
Andrei Matei <a.matei2206@gmail.com>
2024-09-20 09:14:24 +0100

    feat: add admin forms

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .forms import PasskeyForm
+from .models import Passkey
+
+class PasskeyAdmin(UserAdmin):
+    add_form = PasskeyForm
+    model = Passkey
+    list_display = ['username', 'is_staff', 'is_active']
+    list_filter = ['username', 'is_staff', 'is_active']
+
+    fieldsets = (
+        (None, {'fields': ('username', 'password',)}),
+        ('Permissions', {'fields': ('is_staff', 'is_active')}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('password1', 'password2', 'is_staff', 'is_active')}
+        ),
+    )
+
+    search_fields = ['username']
+    ordering = ['username']
+
+admin.site.register(Passkey, PasskeyAdmin)
--- users/views.py
+++ users/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+# from django.contrib.auth import login, logout
+# from django.shortcuts import redirect, render
+# from .forms import PasskeyForm
+# from .models import Passkey
 
-# Create your views here.
+# def logout_view(request):
+#     logout(request)
+#     return redirect("/")
+
+# def login(request):
+#     if request.method == "POST":
+#         form = PasskeyForm(request.POST)
+#         if form.is_valid():
+#             user = form.save()
+#             login(request, user)
+#             return redirect("/")
+#     else:
+#         form = PasskeyForm()
+#     return render(request, "users/login.html", {"form": form})
+
+
